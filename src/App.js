0f40830eed307809660412dfad046276402a6d12
Dheeraj <dyennam@gmail.com>
2020-02-22 11:24:19 -0700

    updated gitignore, added more depedencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
 import Dashboard from './pages/dashboard/Dashboard';
-import ProTip from './ProTip';
+// import ProTip from './ProTip';
 
 export default function App() {
   return (
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -10,14 +10,12 @@ import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
 import Chart from './Chart';
 import Deposits from './Deposits';
@@ -26,7 +24,7 @@ import Orders from './Orders';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {/* {'Copyright © '} */}
       <Link color="inherit" href="https://material-ui.com/">
         Created By Mehindra.
       </Link>{' '}
