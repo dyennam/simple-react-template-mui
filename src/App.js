3cba1bea30f52bc2e46f2f0a05e8187a1cc14701
Dheeraj <dyennam@gmail.com>
2020-02-22 11:27:44 -0700

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import Box from '@material-ui/core/Box';
 import Dashboard from './pages/dashboard/Dashboard';
 // import ProTip from './ProTip';
 
 export default function App() {
   return (
+    <div>
       <Box >
-        <Dashboard />
+      <Router>
+          <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/home" component={Dashboard} />
+          </Switch>
+
+          {/* <Dashboard /> */}
+        </Router>
       </Box>
+    </div>
   );
 }
