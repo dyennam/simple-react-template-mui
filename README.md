feb4b0bbde8f5429e12f8f9ad9387f4041b74b92
Dheeraj <dyennam@gmail.com>
2020-02-22 11:11:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+React UI Project.
+
+
+Material UI (MUI): Is the base library for UI Components
+
+This project uses create-react-app to bootstrap the build / compile process 
+
+Install it and run:
+```sh
+npm install
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import Dashboard from './pages/dashboard/Dashboard';
+import ProTip from './ProTip';
+
+export default function App() {
+  return (
+      <Box >
+        <Dashboard />
+      </Box>
+  );
+}
